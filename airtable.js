b5474d42afcef3221899ce69789cd21c206ab296
Gonçalo Henriques <gonelf@gmail.com>
2021-04-29 23:51:31 +0100

    login, update
    add login
    add cookies
    add airtable

---
+++ airtable.js
@@ -0,0 +1,71 @@
+function getRecordByEmail(email, callback) {
+
+  let formula = "SEARCH('"+email+"',email)";
+  console.log("https://api.airtable.com/v0/apptnwzBVBTiGDyNl/ids?filterByFormula="+formula);
+  $.ajax({
+    url: "https://api.airtable.com/v0/apptnwzBVBTiGDyNl/ids?filterByFormula="+formula,
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorization", "Bearer keybSe3wdoIEJsvGv");
+    },
+    type: 'GET',
+    dataType: 'json',
+    contentType: 'application/json',
+    processData: false,
+    success: function (data) {
+      console.log(JSON.stringify(data));
+      callback(data);
+    },
+    error: function(){
+      console.log("Cannot get data");
+    }
+  });
+}
+
+function getUsersByGID(GID, callback, error) {
+
+  let formula = "(GID="+GID+")";
+  console.log("https://api.airtable.com/v0/apptnwzBVBTiGDyNl/users?filterByFormula="+formula);
+  $.ajax({
+    url: "https://api.airtable.com/v0/apptnwzBVBTiGDyNl/users?filterByFormula="+formula,
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorization", "Bearer keybSe3wdoIEJsvGv");
+    },
+    type: 'GET',
+    dataType: 'json',
+    contentType: 'application/json',
+    processData: false,
+    success: function (data) {
+      console.log(data);
+      callback(data);
+    },
+    error: function(){
+      console.log("Cannot get data");
+      error();
+    }
+  });
+}
+
+function updateUserRecord(record, callback, error) {
+  console.log("https://api.airtable.com/v0/apptnwzBVBTiGDyNl/users");
+  let data = {'records': [record]};
+  console.log(data);
+  $.ajax({
+    url: "https://api.airtable.com/v0/apptnwzBVBTiGDyNl/users",
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorization", "Bearer keybSe3wdoIEJsvGv");
+    },
+    type: 'PATCH',
+    dataType: 'json',
+    contentType: 'application/json',
+    data: JSON.stringify(data),
+    processData: false,
+    success: function (data) {
+      console.log(data);
+      callback(data);
+    },
+    error: function(){
+      console.log("Cannot get data");
+      error();
+    }
+  });
+}
